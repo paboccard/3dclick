706926bafc5ac7172156ce36c6709a881a943d6f
yewed <pa.boccard@gmail.com>
2017-03-14 11:33:30 +0100

    connection + database firebase + update compte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,28 @@ import { SettingsPage } from '../pages/settings/settings';
 import { TabsPage } from '../pages/tabs/tabs';
 import { ModalContentPage } from '../pages/send/send';
 import { ProductPage } from '../pages/product/product';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
+import { ResetpwdPage } from '../pages/resetpwd/resetpwd';
+
+// Import the AF2 Module
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+
+import { AuthService } from '../providers/auth-service';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCC91KCGuqRLddIq39J2WH6L0AZQHy1VHw",
+  authDomain: "dclick-33a50.firebaseapp.com",
+  databaseURL: "https://dclick-33a50.firebaseio.com",
+  storageBucket: "dclick-33a50.appspot.com",
+  messagingSenderId: "903964503324"
+};
+
+const myFirebaseAuthConfig = {
+  provider: AuthProviders.Password,
+  method: AuthMethods.Password
+}
+
 @NgModule({
   declarations: [
     MyApp,
@@ -19,10 +41,14 @@ import { ProductPage } from '../pages/product/product';
     SendPage,
     TabsPage,
     ModalContentPage,
-    ProductPage
+    ProductPage,
+    LoginPage,
+    RegisterPage,
+    ResetpwdPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -34,8 +60,12 @@ import { ProductPage } from '../pages/product/product';
     SendPage,
     TabsPage,
     ModalContentPage,
-    ProductPage
+    ProductPage,
+    LoginPage,
+    RegisterPage,
+    ResetpwdPage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler},
+  AuthService]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 import { ProductPage } from '../product/product';
+import { AuthService } from '../../providers/auth-service';
+
+import firebase from 'firebase';
+import { LoginPage } from '../login/login';
+
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
 //import { starter } from 'ionic-ion-horizontal-scroll-cards';
 
 @Component({
@@ -13,135 +19,73 @@ export class HomePage {
 
 	ctrl : NavController;
 	products : any;
+	theProducts: FirebaseListObservable<any>;
+	theCategories: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, af: AngularFire, public authService: AuthService) {
+
+  	firebase.auth().onAuthStateChanged(function(user) {
+	  if (!user) {
+	    navCtrl.setRoot(LoginPage);
+	  }
+	});
+
   	this.ctrl = navCtrl;
-  	this.products = [
-			{
-				categorie : "En tête d'impression",
-				items :
-				[
-					{
-						title: "GoT",
-						prix: "2,45",
-						description: "Figurine du dragon",
-						imgSrc : "img/img1.jpeg"
-					},
-					{
-						title: "Yugi",
-						prix: "13",
-						description: "Figurine de Yugi-Ho",
-						imgSrc : "img/img1.jpeg"
-					},
-					{
-						title: "Mike",
-						prix: "14,32",
-						description: "Modelisme d'avion",
-						imgSrc : "img/img1.jpeg"
-					},
-					{
-						title: "Chaise",
-						prix: "32,25",
-						description: "Chaise parfaite pour une salle à manger",
-						imgSrc : "img/img1.jpeg"
-					}
-				]
-			},
-			{
-				categorie : "Suggestion",
-				items :
-				[
-					{
-						title: "GoT",
-						prix: "2,45",
-						description: "Figurine du dragon",
-						imgSrc : "img/img1.jpeg"
-					},
-					{
-						title: "Yugi",
-						prix: "13",
-						description: "Figurine de Yugi-Ho",
-						imgSrc : "img/img1.jpeg"
-					},
-					{
-						title: "Mike",
-						prix: "14,32",
-						description: "Modelisme d'avion",
-						imgSrc : "img/img1.jpeg"
-					},
-					{
-						title: "Chaise",
-						prix: "32,25",
-						description: "Chaise parfaite pour une salle à manger",
-						imgSrc : "img/img1.jpeg"
-					}
-				]
-			},
-			{
-				categorie : "Promotion",
-				items :
-				[
-					{
-						title: "GoT",
-						prix: "2,45",
-						description: "Figurine du dragon",
-						imgSrc : "img/img1.jpeg"
-					},
-					{
-						title: "Yugi",
-						prix: "13",
-						description: "Figurine de Yugi-Ho",
-						imgSrc : "img/img1.jpeg"
-					},
-					{
-						title: "Mike",
-						prix: "14,32",
-						description: "Modelisme d'avion",
-						imgSrc : "img/img1.jpeg"
-					},
-					{
-						title: "Chaise",
-						prix: "32,25",
-						description: "Chaise parfaite pour une salle à manger",
-						imgSrc : "img/img1.jpeg"
-					}
-				]
-			},
-			{
-				categorie : "Nouveautés",
-				items :
-				[
-					{
-						title: "GoT",
-						prix: "2,45",
-						description: "Figurine du dragon",
-						imgSrc : "img/img1.jpeg"
-					},
-					{
-						title: "Yugi",
-						prix: "13",
-						description: "Figurine de Yugi-Ho",
-						imgSrc : "img/img1.jpeg"
-					},
-					{
-						title: "Mike",
-						prix: "14,32",
-						description: "Modelisme d'avion",
-						imgSrc : "img/img1.jpeg"
-					},
-					{
-						title: "Chaise",
-						prix: "32,25",
-						description: "Chaise parfaite pour une salle à manger",
-						imgSrc : "img/img1.jpeg"
-					}
-				]
-			}
-		]
+  	this.theCategories = af.database.list('/categories');
+  	this.theProducts = af.database.list('/products');
+  	
   }
 
   goToProduct(product){
   	this.ctrl.push(ProductPage, product);
+  }
+
+  logout() {
+	  this.authService.doLogout();
+	}
+
+  createProducts(af : AngularFire){
+  	this.theProducts = af.database.list('/products');
+  	this.theProducts.push({
+		title: "GoT",
+		prix: "2,45",
+		description: "Figurine du dragon",
+		imgSrc : "img/img1.jpeg"
+  	});
+  	this.theProducts.push({
+		title: "Yugi",
+		prix: "13",
+		description: "Figurine de Yugi-Ho",
+		imgSrc : "img/img1.jpeg"
+  	});
+  	this.theProducts.push({
+		title: "Mike",
+		prix: "14,32",
+		description: "Modelisme d'avion",
+		imgSrc : "img/img1.jpeg"
+  	});
+  	this.theProducts.push({
+		title: "Chaise",
+		prix: "32,25",
+		description: "Chaise parfaite pour une salle à manger",
+		imgSrc : "img/img1.jpeg"
+  	});
+  }
+
+  createCategories(af: AngularFire){
+  	this.theCategories = af.database.list('/categories');
+  	this.theCategories.push({
+			categorie : "En tête d'impression"
+	});
+	this.theCategories.push({
+			categorie : "Nouveautés"
+	});
+	this.theCategories.push({
+			categorie : "Promotion"
+	});
+	this.theCategories.push({
+			categorie : "Suggestion"
+	}); 
   }
 
 }
